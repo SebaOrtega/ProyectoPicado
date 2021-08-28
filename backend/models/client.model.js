0b1f1d22b9327ea1be82f5a01e1a3c4cd76182bc
Spyblack23 <86201907+Spyblack23@users.noreply.github.com>
2021-08-28 16:16:27 -0300

    Database conn
    Database queries

---
+++ backend/models/client.model.js
@@ -0,0 +1,17 @@
+const mongoose = require ('mongoose');
+
+const Schema = mongoose.Schema;
+
+const clientSchema = new Schema({
+    nombre_cliente: {type: String,required: true},
+    apellido_cliente: {type: String,required: true},
+    mail: {type: String,required: true},
+    password: {type: String,required: true},
+    rep_pw: {type: String,required: true},
+},{
+        timestamps: true,
+})
+
+const Client = mongoose.model('Client', clientSchema)
+
+module.exports = Client;
---
+++ backend/routes/clients.js
@@ -0,0 +1,52 @@
+const router = require('express').Router();
+let Client = require('../models/client.model')
+
+router.route('/').get((req,res) => {
+    Client.find()
+    .then(clients => res.json(clients))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req, res) => {
+    const nombre_cliente = req.body.nombre_cliente;
+    const apellido_cliente = req.body.apellido_cliente;
+    const mail = req.body.mail;
+    const password = req.body.password;
+    const rep_pw = req.body.rep_pw;
+
+    const newClient = new Client({nombre_cliente, apellido_cliente, mail, password, rep_pw});
+
+    newClient.save()
+    .then(() => res.json('¡Client added!'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').get((req,res) => {
+    Client.findById(req.params.id)
+    .then(client => res.json(client))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req,res) => {
+    Client.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Client deleted'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) => {
+    Client.findById(req.params.id)
+    .then(client => {
+        client.nombre_cliente = req.body.nombre_cliente;
+        client.apellido_cliente = req.body.apellido_cliente;
+        client.mail = req.body.mail;
+        client.password = req.body.password;
+        client.rep_pw = req.body.rep_pw;
+
+        client.save()
+        .then(() => res.json('Client updated'))
+        .catch(err => res.stats(400).json('Error: ' + err));
+    })
+    .catch(err => res.stats(400).json('Error: ' + err))
+});
+
+module.exports = router;
